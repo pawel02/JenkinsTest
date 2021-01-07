77be3efb8ab20d74457de556775b10850f7c0c87
pawel02 <pawel.bes13@gmail.com>
2021-01-07 09:02:27 +0000

    test

--- src/app/root/services/test.service.spec.ts
+++ src/app/root/services/test.service.spec.ts
@@ -31,7 +31,7 @@ describe('TestService', () => {
 
   it('Get Observable', (done: DoneFn)=>{
     service.sObs.subscribe(value => {
-      expect(value).toBe('Something');
+      expect(value).toBe('omething');
       done();
     });
     service.next();
